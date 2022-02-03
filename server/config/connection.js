b9cb4a40ddd850a153023bf777d2be31d8c3ebd0
kperkins97 <perkins.kelsey@yahoo.com>
2022-02-03 16:32:52 -0600

    trying to deploy again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/books', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -8,3 +8,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/books', {
 });
 
 module.exports = mongoose.connection;
+
+
+
